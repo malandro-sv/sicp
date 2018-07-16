66ce0c10f7dbe9d70b12db49dc94121d8f53474a
Nam Lah <nam-lah@users.noreply.github.com>
2018-07-16 16:10:56 -0600

    Create auxiliars.scm

---
+++ misc/auxiliars.scm
@@ -0,0 +1,31 @@
+(define (square x) (* x x))
+(define (cube x) (* x x x))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (smallest-divisor n)
+  (find-divisor n 2)) 
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+  		((divides? test-divisor n) test-divisor)
+		(else (find-divisor n (next test-divisor))))) 
+
+(define (next number)
+  (if (even? number) (inc number)
+	  (inc-odd number)))
+    
+(define (list-sum term lst)
+  (if (null? lst) 0
+	  (+ (term (car lst))
+		 (list-sum term (cdr lst)))))
+
+(define (list-average lst)
+  (/ (sum + lst)
+	 (length lst)))
+
+(define (sum term a next b)
+  (if (> a b) 0
+	  (+ (term a)
+		 (sum term (next a) next b))))
