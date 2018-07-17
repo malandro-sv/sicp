a568787a98bbcbf04f408e3940ccce400693eb2b
Nam Lah <nam-lah@users.noreply.github.com>
2018-07-17 00:27:16 -0600

    Update 1.17.scm

--- 1.17.scm
+++ 1.17.scm
@@ -1,9 +1,29 @@
-;; sicp 1.17
+;; 1.17                                                                                                     
+;; Multiplication by repeated addition.
+;; generates a recursive computation.
 
-(define (multiply a b)
+;; Linear, recursive example,
+;; (May be improved as well:
+(define (example-multiply a b)
   (if (= b 0) 0
-  (+ a (multiply a (- b 1)))))
+  (+ a (example-multiply a (- b 1)))))
 
+;; Improved, linear-recursive procedure;
+;; Includes multiply-by-0-or-by-1 cases,
+;; to avoid recursing unnecessarily: 
+(define (multiply a b)
+  (cond ((= a 1) b)
+        ((= b 1) a)
+        ((= b 0) 0)
+        (else (mult a b))))
+
+(define (mult a b)
+  (if (= b 0) 0
+      (+ a (mult a
+                 (- b 1)))))
+
+;; this is the logarithmic,
+;; iterative procedure:
 (define (iter-multiply a b)
   (cond ((= b 0) 0)
         ((= b 1) a)
