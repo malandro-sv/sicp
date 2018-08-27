844e9a51a7949b2e6c81abd0219dbef252efbb0e
Nam Lah <nam-lah@users.noreply.github.com>
2018-08-27 14:28:29 -0600

    Update 2.1.4-sketch.scm

--- 2.1.4-sketch.scm
+++ 2.1.4-sketch.scm
@@ -8,36 +8,15 @@
     (define (bound operator)
         (operator ohm (ratio ohm percent)))  
     
- 11     (cons (bound -)
-(load "../misc/auxiliars.scm") 
-          
-(define (ratio ohm percent)
-    (* ohm (/ percent 100)))
-    
-;; I initially called this, "tolerance":
-(define (make-interval ohm percent)
-    (define (bound operator)
-        (operator ohm (ratio ohm percent)))
-    
-    (cons (bound -)
-(load "../misc/auxiliars.scm") 
-
-(define (ratio ohm percent)
-    (* ohm (/ percent 100)))
-
-;; I initially called this, "tolerance":
-(define (make-interval ohm percent)
-    (define (bound operator)
-        (operator ohm (ratio ohm percent)))
-    
     (cons (bound -)
           (bound +))) 
-           
+                     
 ;; total rsistance for two items only: 
 (define (pair-allel r1 r2) 
     (/ 1 (+ (/ 1 r1) 
             (/ 1 r2)))) 
  
+;; total resistance value
 ;; for a list of resitors: 
 (define (parallel-denominator Rs) 
   (if (null? Rs) 0 
@@ -58,6 +37,7 @@
 (define (lower-bound interval) 
   (car interval)) 
  
+ ;; make-interval thru message-passing.
 (define (bound interval) 
   (define (selector option) 
     (cond ((eq? option 'lower)  (car interval)) 
